7979f366e5635463d9d50b0a573e8502dd010f9e
Wanjiang Xie <wanjiang.xie@ndsu.edu>
2025-03-19 17:46:12 -0500

    Fixed merge conflicts and cleaned up app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],  // 这里包含 HomeComponent
+  imports: [RouterOutlet, HomeComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
