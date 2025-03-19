0925ba14b5e85a1a8cfd9f211ecde6148b2c6554
Wanjiang Xie <wanjiang.xie@ndsu.edu>
2025-03-19 18:39:52 -0500

    Added HousingLocation Interface and created a single instance of the new interface

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
   templateUrl: './housing-location.component.html',
-  styleUrl: './housing-location.component.css'
+  styleUrls: ['./housing-location.component.css']
 })
-export class HousingLocationComponent {
-
-}
+export class HousingLocationComponent { }
