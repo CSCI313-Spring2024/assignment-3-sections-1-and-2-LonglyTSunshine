736cf51de48e145b3fd52b982e7b6295a74286b3
Wanjiang Xie <wanjiang.xie@ndsu.edu>
2025-03-19 20:48:27 -0500

    Added routing to the angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent], 
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
