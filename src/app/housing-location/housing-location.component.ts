a0b655c037f71d5c5900dca55885d19b6110c886
Wanjiang Xie <wanjiang.xie@ndsu.edu>
2025-03-19 19:00:11 -0500

    Added Inputs to the HousingLocationComponent

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+
+
 
 @Component({
   selector: 'app-housing-location',
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })
-export class HousingLocationComponent { }
+
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
